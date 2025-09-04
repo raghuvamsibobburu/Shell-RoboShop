02d2e17c4868c9323278e1c1fbc2197212396753
RaghuVamsiB <raghuvamsibobburu@gmail.com>
2025-09-04 10:50:00 +0530

    configuring instances

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "payment" "dispatch"
 ZONE_ID="Z0630864SNPS171VLZQ6"
 DOMAIN_NAME="buyarobot.site"
 
-for instance in ${INSTANCES[@]}
+#for instance in ${INSTANCES[@]}
+for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0abe24d1b55914caa --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
@@ -18,4 +19,24 @@ do
 
     fi
     echo "$instance IP address: $IP"
+    
+    #AWS cli to create or update route 53 records
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$instance'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
 done 
