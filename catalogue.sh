d583fe58dd6557f5efa06cf9f85259dc68d7b1d2
RaghuVamsiB <raghuvamsibobburu@gmail.com>
2025-09-12 16:48:05 +0530

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -77,11 +77,5 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
-STATUS=$(mongosh --host mongodb.buyarobot.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-if [ $STATUS -lt 0 ]
-then
-    mongosh --host mongodb.buyarobot.site </app/db/master-data.js &>>$LOG_FILE
-    VALIDATE $? "Loading data into MongoDB"
-else
-    echo -e "Data is already loaded ... $Y SKIPPING $N"
-fi
+mongosh --host mongodb.buyarobot.site </app/db/master-data.js &>>$LOG_FILE
+VALIDATE $? "Loading data into MongoDB"
