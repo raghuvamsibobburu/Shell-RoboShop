d774a2e4ae3af855d2ee1582543c4fc0f074e145
RaghuVamsiB <raghuvamsibobburu@gmail.com>
2025-09-12 11:51:00 +0530

    roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -9,7 +9,7 @@ RESET="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-PACKAGES=("mysql" "python3" "nginx" "httpd" "" "")
+SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at $(date)" | tee -a $LOG_FILE
@@ -27,50 +27,55 @@ fi
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo -e "Installing $2 is.......$GREEN SUCCESS $RESET" | tee -a $LOG_FILE
+        echo -e "$2 is.......$GREEN SUCCESS $RESET" | tee -a $LOG_FILE
     else
-        echo -e "Installing $2 is.......$RED FAILURE $RESET" | tee -a $LOG_FILE
+        echo -e "$2 is.......$RED FAILURE $RESET" | tee -a $LOG_FILE
         exit 1
     fi
 }
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$LOG_FILE
 VALIDATE $? "Disabling nodeJs"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>$LOG_FILE
 VALIDATE $? "Enabling nodeJs version:20"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing nodeJs"
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
 VALIDATE $? "Creating roboshop system user"
 
 mkdir /app 
 VALIDATE $? "Creating app directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading Catalogue"
 
 rm -rf /app/*
 cd /app 
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>$LOG_FILE
 VALIDATE $? "unzipping catalogue"
 
-npm install
+npm install &>>$LOG_FILE
 VALIDATE $? "Installing Dependencies"
 
-systemctl daemon-reload
-systemctl enable catalogue 
-systemctl start catalogue
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copying catalogue service"
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable catalogue &>>$LOG_FILE
+systemctl start catalogue 
 VALIDATE $? "Starting catalogue"
 
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB client"
 
-VALIDATE $? ""
+mongosh --host mongodb.buyarobot.site </app/db/master-data.js &>>$LOG_FILE
+VALIDATE $? "Loading Data into MongoDB"
 
 
-
-VALIDATE $? ""
 
 
 
